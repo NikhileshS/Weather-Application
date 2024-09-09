13d4aff7ab34044050f52eb17e64ec34912c0374
NikhileshS <nikhileshs2004@gmail.com>
2024-09-09 17:39:33 +0530

    Initialized Collection of data from the API

---
+++ src/getdata.js
@@ -0,0 +1,24 @@
+
+const getdataC = async(place) =>{
+    try{
+        const response = fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=metric&key=7DT8JRFCND5WFYNR3RXFDWCJ9&contentType=json`,{mode:'cors'})
+        const data = (await response).json()
+        return data
+    }
+    catch(error){
+        console.error('BAD API LOCATION ERROR', error)
+    }
+}
+
+const getdataF = async(place) =>{
+    try{
+        const response = fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${place}?unitGroup=us&key=7DT8JRFCND5WFYNR3RXFDWCJ9&contentType=json`,{mode:'cors'})
+        const data = (await response).json()
+        return data
+    }
+    catch(error){
+        console.error('BAD API LOCATION ERROR', error)
+    }
+}
+
+export {getdataC,getdataC}
